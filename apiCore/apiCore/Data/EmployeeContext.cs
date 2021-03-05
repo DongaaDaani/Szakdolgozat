86edd1b567adc8d17b7bae8b3b486d27a404b295
DongaDani <donga.dani99@gmail.com>
2021-03-05 20:30:47 +0100

    Fix Cross Problem, Change request methods, + model

---
+++ apiCore/apiCore/Data/EmployeeContext.cs
@@ -0,0 +1,47 @@
+﻿using apiCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiCore.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        //adatbázis tábla létrehozása
+
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Duty> Task { get; set; }
+        public DbSet<Leader> Leader { get; set; }
+
+        public EmployeeContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //reláció 1:1 1:n n:m
+
+            modelBuilder.Entity<Employee>()
+            .HasOne(p => p.Deprartment)
+            .WithMany(b => b.Employee)
+            .HasForeignKey(ug => ug.DepartmentId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Duty>()
+            .HasKey(bc => new { bc.EmployeeId, bc.LeaderId });
+            modelBuilder.Entity<Duty>()
+                .HasOne(bc => bc.Employee)
+                .WithMany(b => b.Tasks)
+                .HasForeignKey(bc => bc.EmployeeId);
+            modelBuilder.Entity<Duty>()
+                .HasOne(bc => bc.Leader)
+                .WithMany(c => c.Tasks)
+                .HasForeignKey(bc => bc.LeaderId);
+
+            modelBuilder.Entity<Department>()
+             .HasOne(a => a.Leader).WithOne(b => b.Department)
+             .HasForeignKey<Leader>(e => e.DepartmentId);
+
+        }
+    }
+}
---
+++ apiCore/apiCore/Models/Maintance.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiCore.Models
+{
+    public class Maintance
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MaintanceId { get; private set; }
+        public string Plate { get; private set; }
+        public string Brand { get; private set; }
+        public string Type { get; private set; }
+        public string Model { get; private set; }
+
+        public int Km { get; private set; }
+        public int OperationHours { get; private set; }
+
+        public string ProblemDescription { get; private set; }
+        public string SupervisorDescription { get; private set; }
+        public string CoordinatorDescription { get; private set; }
+        public string MechanicalDescription { get; private set; }
+
+        public int EmpolyeeId { get; set; } // FOREIGN KEY
+
+        public Maintance(int maintanceId, string plate, string brand,
+         string type, string model, int km, int operationHours, string problemDescription,
+         string supervisorDescription, string coordinatorDescription, string mechanicalDescription,
+         int empolyeeId)
+        {
+            MaintanceId = maintanceId;
+            Plate = plate;
+            Brand = brand;
+            Type = type;
+            Model = model;
+            Km = km;
+            OperationHours = operationHours;
+            ProblemDescription = problemDescription;
+            SupervisorDescription = supervisorDescription;
+            CoordinatorDescription = coordinatorDescription;
+            MechanicalDescription = mechanicalDescription;
+            EmpolyeeId = empolyeeId;
+        }
+
+    }
+}
